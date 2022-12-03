4c74c51bbdae6a75cf63b965bc888f9aa29d3e1f
Vydhushya Sonaleo Mandapati <72376504+vydhushya@users.noreply.github.com>
2022-12-03 15:10:50 +0530

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function hello(){
+    console.log("Hello ");
     console.log("Hello Node.JS! Trying it for the first time");
-    console.log("Hello Node.JS! Trying it for the first time");
+
 }
 
 hello();
