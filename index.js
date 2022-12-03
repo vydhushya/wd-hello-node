dff160adb05c19658d1a15a9bf9d4b4022bf2027
Vydhushya Sonaleo Mandapati <72376504+vydhushya@users.noreply.github.com>
2022-12-04 00:40:12 +0530

    used streams

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
+const http = require("http");
 const fs = require("fs");
-fs.writeFile(
-  "sample.txt",
-  "Hello World. Welcome to Node.js File system module.",
-  (err) =>{
-    if (err) throw err;
-    console.log("File created");
-  }
-);
 
-fs.readFile("sample.txt", (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
+const server = http.createServer((req,res) => {
+    /*fs.readFile("sample.txt", (err, data) => {
+        res.end(data);
+    })*/
+    const stream = fs.createReadStream("test.txt");
+    stream.pipe(res);
 });
-
-fs.appendFile("sample.txt", " This is my updated content", (err) => {
-  if (err) throw err;
-  console.log("File updated!");
-});
-
-fs.rename("sample.txt", "test.txt", (err) => {
-  if (err) throw err;
-  console.log("File name updated!");
-});
-
-fs.unlink("test.txt", (err) => {
-  if (err) throw err;
-  console.log("File test.txt deleted successfully!");
-});
+server.listen(3000);
