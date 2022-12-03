09caa98498f86802f5b72b89b0283288efc05e56
Vydhushya Sonaleo Mandapati <72376504+vydhushya@users.noreply.github.com>
2022-12-04 00:27:16 +0530

    functions using fs

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const returnHello = () => {
-  console.log('Hello Node.js!')
-}
+const fs = require("fs");
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File system module.",
+  (err) =>{
+    if (err) throw err;
+    console.log("File created");
+  }
+);
 
-returnHello()
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+fs.appendFile("sample.txt", " This is my updated content", (err) => {
+  if (err) throw err;
+  console.log("File updated!");
+});
+
+fs.rename("sample.txt", "test.txt", (err) => {
+  if (err) throw err;
+  console.log("File name updated!");
+});
+
+fs.unlink("test.txt", (err) => {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});
