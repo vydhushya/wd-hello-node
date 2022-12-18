4732f8abdc66564d47541ec8cf1d41089951271d
Vydhushya Sonaleo Mandapati <72376504+vydhushya@users.noreply.github.com>
2022-12-18 17:31:13 +0530

    added jest,husky dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const todoList = () => {
-  all = []
+  let all = []
   const add = (todoItem) => {
     all.push(todoItem)
   }
@@ -80,49 +80,10 @@ const todoList = () => {
   
 
   return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
-}
+};
 
-// ####################################### #
-// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-// ####################################### #
+module.exports = todoList;
 
-const todos = todoList();
 
-const formattedDate = d => {
-  return d.toISOString().split("T")[0]
-}
 
-var dateToday = new Date()
-const today = formattedDate(dateToday)
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-)
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-)
-
-todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-
-console.log("My Todo-list\n\n")
-
-console.log("Overdue")
-var overdues = todos.overdue()
-var formattedOverdues = todos.toDisplayableList(overdues)
-console.log(formattedOverdues)
-console.log("\n\n")
-
-console.log("Due Today")
-let itemsDueToday = todos.dueToday()
-let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-console.log(formattedItemsDueToday)
-console.log("\n\n")
-
-console.log("Due Later")
-let itemsDueLater = todos.dueLater()
-let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-console.log(formattedItemsDueLater)
-console.log("\n\n")
+  
