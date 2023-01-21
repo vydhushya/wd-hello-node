0692e8222aa6d01cd404853dd43dc8ace892ed46
Vydhushya Sonaleo Mandapati <72376504+vydhushya@users.noreply.github.com>
2023-01-21 19:28:49 +0530

    modified

--- completeTodo.js
+++ completeTodo.js
@@ -1,6 +1,6 @@
 // completeTodo.js
 var argv = require('minimist')(process.argv.slice(2));
-const db = require("./models/index");
+const db = require("./models/index.js");
 const markAsComplete = async (id) => {
   try {
     await db.Todo.markAsComplete(id);
